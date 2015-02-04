2b17af338473683ee768db514f0730960291c567
srtk <kurotaki888@gmail.com>
2015-02-04 11:19:06 +0900

    use new json (in progress)

--- src/js/news20_sda.js
+++ src/js/news20_sda.js
@@ -1,20 +1,36 @@
 
-var data = null
-var finish_loading = false
-var data_index = 0
 
 var classes_txt = [
-  'comp.graphics', 
-  'comp.os.ms-windows.misc',
-  'comp.sys.ibm.pc.hardware',
+  "comp.graphics",
+  "comp.os.ms-windows.misc",
+  "comp.sys.ibm.pc.hardware",
+  "comp.sys.mac.hardware",
+  "comp.windows.x",
+  "rec.autos",
+  "rec.motorcycles",
+  "rec.sport.baseball",
+  "rec.sport.hockey",
+  "sci.crypt",
+  "sci.electronics",
+  "sci.med",
+  "sci.space",
+  "misc.forsale",
+  "talk.politics.misc",
+  "talk.politics.guns",
+  "talk.politics.mideast	",
+  "talk.religion.misc",
+  "alt.atheism",
+  "soc.religion.christian"
 ]
 
 $(window).load(function() {
-  load_json("data/dummy.json");
+  load_json("data/dummy_small.json");
   start_fun();
 });
 
 
+var data = null
+var finish_loading = false
 var load_json = function(filename) {
   $.getJSON(filename, function(json) {
     console.log(json);
@@ -23,23 +39,9 @@ var load_json = function(filename) {
   });
 }
 
-var next_data = function() {
-  next = null;
-  if (data_index < data.error_rate.length) {
-    next = {}; 
-    next.error_rate = data['error_rate'][data_index]
-    next.predict = data['predict'][data_index]
-    next.answer = data['answer'][data_index]
-    next.rgb = data['rgb'][data_index]
-    data_index += 1;
-  }
-  return next
-}
-
 var start_fun = function() {
   if (finish_loading) {
     console.log('finish loading json!');
-    change_document(data['document'])
     setInterval(step, 1000);  
   } else {
     setTimeout(start_fun, 1000)
@@ -52,39 +54,79 @@ var step = function() {
   if (paused) {
     return;
   }
-  next = next_data()
-  if (next) {
-    process_data(next);
-    // log progress to graph, (full loss)
+  next_epoch = next_epoch_data()
+  if (next_epoch) {
+    process_epoch(next_epoch)
   } else {
-    paused = true;
-    return;
+    next_doc = next_document()
+    if (next_doc) {
+      change_document(next_doc)
+    } else {
+      paused = true;
+      return;
+    }
   }
 }
 
+var doc_index = 0
+var next_document = function() {
+  next = null;
+  if (doc_index < data.error_rate.length) {
+    next = {};
+    next.document = data['document'][doc_index]
+    next.error_rate = data['error_rate'][doc_index]
+    next.predict = data['predict'][doc_index]
+    next.answer = data['answer'][doc_index]
+    next.rgb = data['rgb'][doc_index]
+    doc_index += 1;
+  }
+  return next
+}
+
+var split_doc = null;
+var predict_doc = null;
+var rgb_doc = null;
 var lossGraph = new cnnvis.Graph({step_horizon:20});
 var step_num = 0;
-var process_data = function(d) {
-  console.log('step '+step_num);
-  console.log(d);
-  lossGraph.add(step_num, d.error_rate);
+var change_document = function(doc) {
+  $('#documents').text(doc);
+  //split_doc = doc.replace(/\W+/g, ' ').split(' ');
+  split_doc = doc.split(/\W+/)
+  
+  lossGraph.add(step_num, doc.error_rate);
   lossGraph.drawSelf($("#lossgraph")[0]);
+  
+  $("#answer").text(classes_txt[doc.answer]);
+  
+  predict_doc = doc.predict;
+  rgb_doc = doc.rgb;
+}
 
-  $("#prediction").text(classes_txt[d.predict]);
-  $("#answer").text(classes_txt[d.answer]);
+var epoch_num = 0;
+var next_epoch_data = function() {
+  if (!predict_doc || !rgb_doc) {
+    return null;
+  }
+  data = {};
+  data.predict = predict_doc[epoch_num];
+  data.rgb = rgb_doc[epoch_num];
+  epoch_num++;
+  return data;
+}
+
+var colors = null;
+var process_data = function(data) {
+  $("#prediction").text(classes_txt[data.predict]);
   
   scale = 255  // convert interval [0.0, 1.0] to [0, 255]
-  r = Math.ceil(d.rgb[0] * scale);
-  g = Math.ceil(d.rgb[1] * scale);
-  b = Math.ceil(d.rgb[2] * scale);
-  $("#documents").css("background-color", "rgb("+r.toString()+","+g.toString()+","+b.toString()+")");
-  
+  len = split_doc.length;
+  for (var i = 0; i < len; i++) {
+    r = Math.ceil(data.rgb[0] * scale);
+    g = Math.ceil(data.rgb[1] * scale);
+    b = Math.ceil(data.rgb[2] * scale);
+    $("#documents").css("background-color", "rgb("+r.toString()+","+g.toString()+","+b.toString()+")");
+  }
   step_num++;
 }
 
-
-var colors = null;
-var change_document = function(doc) {
-  $('#documents').text(doc);
-}
 
